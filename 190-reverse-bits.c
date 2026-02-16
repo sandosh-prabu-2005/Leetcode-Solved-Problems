7344882838c591d8c2ca33f65d9db65755e44d13
Sandosh Prabu G <hhi050844@gmail.com>
2026-02-16 14:10:25 +0530

    Solved Problems

---
+++ 190-reverse-bits.c
@@ -0,0 +1,44 @@
+void decimalToBinaryString(unsigned int n, char *binaryStr) {
+    for (int i = 31; i >= 0; i--) {
+        binaryStr[i] = (n % 2) + '0';
+        n /= 2;
+    }
+    binaryStr[32] = '\0';
+}
+
+// 2. Reverse the String (Equivalent to your reverseNumber)
+void reverseString(char *str) {
+    int i = 0, j = strlen(str) - 1;
+    while (i < j) {
+        char temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+        i++;
+        j--;
+    }
+}
+
+// 3. Convert Binary String back to Decimal
+unsigned int binaryStringToDecimal(char *binaryStr) {
+    unsigned int decimal = 0;
+    for (int i = 0; i < 32; i++) {
+        if (binaryStr[i] == '1') {
+            // Using bitwise shift for power of 2: (1 << (31 - i))
+            decimal += (unsigned int)pow(2, 31 - i);
+        }
+    }
+    return decimal;
+}
+
+uint32_t reverseBits(uint32_t n) {
+    uint32_t result = 0;
+    for (int i = 0; i < 32; i++) {
+        result <<= 1;
+        if (n & 1) {
+            result++;
+        }
+        n >>= 1;
+    }
+    
+    return result;
+}
